d221c7f2200627539c8064387e398e3189a08a99
rasooly-dev <alofiabdulprogramming@gmail.com>
2022-08-10 04:56:11 -0700

    Document Accounts Related Tasks

--- src/Accounts/accounts.js
+++ src/Accounts/accounts.js
@@ -6,59 +6,99 @@ const database = require('../utils/database')
 
 // methods
 
+/**
+ * Stores the accounts of a user in the database
+ * 
+ * @param {Object} user an object holding the user's id and username
+ * @param {String} accounts an encrypted string of the user's accounts
+ * @returns database related callback
+ * 
+ * @throws {NoAccountsProvidedException} if no accounts were provided
+ */
 const storeAccounts = async (user, accounts) => {
+    // grab the user's id
     const { id } = user
 
+    // if no accounts were provided, throw an exception
     if (accounts === undefined || accounts === null) 
         throw new NoAccountsProvidedException('No accounts provided')
 
+    // if the account string is empty, set it to an empty string
     if (accounts === "") 
         return await database.update('users', { accounts: accounts }, { id })
 
+    // otherwise, encrypt the account string and update the database
     let accounts_to_store = encrypt(accounts)
     return await database.update('users', { accounts: accounts_to_store }, { id })
 }
 
+/**
+ * Gets the accounts of a user from the database
+ * 
+ * @param {Object} user an object holding the user's id and username
+ * @returns accounts of the user
+ */
 const getAccounts = async (user) => {
+    // grab the user's id
     const { id } = user
 
+    // get the user's accounts from the database
     const user_accounts = await database.query('SELECT accounts FROM users WHERE id = $1', [id])
 
+    // if the user has no accounts, return an empty string
     if (user_accounts.rows[0].accounts === "") 
         return ""
 
+    // otherwise, decrypt the user's accounts and return them
     const accounts = decrypt(user_accounts.rows[0].accounts)
-
-
     return accounts
 }
 
 
 // utils
 
+/**
+ * Encrypts the accounts string with the company secret
+ * 
+ * @param {String} accountText string containing the accounts to encrypt
+ * @returns encrypted string using the company secret
+ */
 const encrypt = (accountText) => {
+    // encrypt the account string with the company secret and return it
     return aes256.encrypt(process.env.ACCOUNT_COMPANY_SECRET, accountText)
 }
 
+/**
+ * Decrypts the accounts string with the company secret
+ * 
+ * @param {String} accountText an encrypted string containing the accounts to decrypt
+ * @returns decrypted string using the company secret
+ */
 const decrypt = (accountText) => {
+    // decrypt the account string with the company secret and return it
     return aes256.decrypt(process.env.ACCOUNT_COMPANY_SECRET, accountText)
 }
 
-const test = async () => {
-    await storeAccounts({ id: 2 }, "accounts_test")
-    console.log(await getAccounts({id: 2}))
-}
+// TEST METHOD - DO NOT USE
+// const test = async () => {
+//     await storeAccounts({ id: 2 }, "accounts_test")
+//     console.log(await getAccounts({id: 2}))
+// }
 
 
 // exceptions
 
+/**
+ * Exception thrown when no accounts were provided
+ * 
+ * @param { String } message message to be displayed when the exception is thrown
+ */
 function NoAccountsProvidedException (message) {
     this.message = message
     this.name = 'NoAccountsProvidedException'
 }
 
 // exports
-
 module.exports = {
     storeAccounts,
     getAccounts,
