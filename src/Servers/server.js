060f274cd9aeaa0f3b47e5e63dbe2ddaf20458db
rasooly-dev <alofiabdulprogramming@gmail.com>
2022-08-21 00:38:44 -0700

    Update Server Cors Options

--- src/Servers/server.js
+++ src/Servers/server.js
@@ -4,11 +4,12 @@ const app = express()
 const auth_routes = require('../Routes/auth_routes')
 const acc_routes = require('../Routes/acc_routes')
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'http://localhost:3000',
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200
+const cors = require("cors");
+
+const corsOptions = {
+   origin: 'http://localhost:3000',
+   credentials: true,            //access-control-allow-credentials:true
+   optionSuccessStatus: 200
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
